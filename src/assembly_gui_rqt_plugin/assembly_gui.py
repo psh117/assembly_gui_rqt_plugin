38e40ade84fe3fd562eccf5a2dd90c2376649e30
kim-hc <joel9537@gmail.com>
2020-11-24 20:44:19 +0900

    error fixed - action call

--- src/assembly_gui_rqt_plugin/assembly_gui.py
+++ src/assembly_gui_rqt_plugin/assembly_gui.py
@@ -20,19 +20,19 @@ from controller_manager_msgs.srv import SwitchController, SwitchControllerReques
 from assembly_dxl_gripper.srv import Move, MoveRequest
 import actionlib
 from actionlib_msgs.msg import GoalStatus
-from assembly_task_manager.params.default_parameters import short_bolt_drill_load, long_bolt_drill_load, side_left_load, side_right_load
-from assembly_task_manager.params.default_parameters import long_load, short_load, middle_load, bottom_load, chair_load
+# from assembly_task_manager.params.default_parameters import short_bolt_drill_load, long_bolt_drill_load, side_left_load, side_right_load
+# from assembly_task_manager.params.default_parameters import long_load, short_load, middle_load, bottom_load, chair_load
 
-# ## when testing -- not using default_parameters
-# short_bolt_drill_load = SetLoadRequest(mass=0.0, F_x_center_load = [0.0, 0.0, 0.0])
-# long_bolt_drill_load = short_bolt_drill_load
-# side_left_load = short_bolt_drill_load
-# side_right_load = short_bolt_drill_load
-# long_load = short_bolt_drill_load
-# short_load = short_bolt_drill_load
-# middle_load = short_bolt_drill_load
-# bottom_load = short_bolt_drill_load
-# chair_load = short_bolt_drill_load
+## when testing -- not using default_parameters
+short_bolt_drill_load = SetLoadRequest(mass=0.0, F_x_center_load = [0.0, 0.0, 0.0])
+long_bolt_drill_load = short_bolt_drill_load
+side_left_load = short_bolt_drill_load
+side_right_load = short_bolt_drill_load
+long_load = short_bolt_drill_load
+short_load = short_bolt_drill_load
+middle_load = short_bolt_drill_load
+bottom_load = short_bolt_drill_load
+chair_load = short_bolt_drill_load
 
 class AssemblyGuiPlugin(Plugin):
 
@@ -288,15 +288,12 @@ class AssemblyGuiPlugin(Plugin):
     def btn_left_gripper_open_on_click(self):
         req = MoveGoal(width=0.075,speed=0.1)
 
-        if self.panda_gripper_grasp_client.get_state() is GoalStatus.ACTIVE:
+        if self.panda_gripper_grasp_client.wait_for_server(rospy.Duration(1.0)):
             self.panda_gripper_move_client.send_goal_and_wait(req)
             self._widget.textBrowser.append("LEFT: gripper_open")
+        else:
+            self._widget.textBrowser.append("SERVER NOT WORKING -- LEFT: gripper_open")
 
-        elif self.panda_gripper_grasp_client.get_state() is GoalStatus.LOST:
-            self._widget.textBrowser.append("SERVER LOST -- LEFT: gripper_open")
-
-        elif self.panda_gripper_grasp_client.get_state() is GoalStatus.PENDING:
-            self._widget.textBrowser.append("SERVER PENDING -- LEFT: gripper_open")
 
     def btn_left_gripper_close_on_click(self):
         force = self._widget.spinbox_left_gripper_force.value()
@@ -306,15 +303,12 @@ class AssemblyGuiPlugin(Plugin):
                           epsilon=epsilon,
                           speed=0.1,
                           force=force)
-        if self.panda_gripper_grasp_client.get_state() is GoalStatus.ACTIVE:
+        if self.panda_gripper_grasp_client.wait_for_server(rospy.Duration(1.0)):
             self.panda_gripper_grasp_client.send_goal_and_wait(req)
             self._widget.textBrowser.append("LEFT: gripper_close | force: "+str(force)+" | length: "+str(length))
 
-        elif self.panda_gripper_grasp_client.get_state() is GoalStatus.LOST:
-            self._widget.textBrowser.append("SERVER LOST -- LEFT: gripper_open")
-
-        elif self.panda_gripper_grasp_client.get_state() is GoalStatus.PENDING:
-            self._widget.textBrowser.append("SERVER PENDING -- LEFT: gripper_open")
+        else:
+            self._widget.textBrowser.append("SERVER NOT WORKING -- LEFT: gripper_open")
 
 
     def btn_drill_long_run_on_click(self):
@@ -375,7 +369,7 @@ class AssemblyGuiPlugin(Plugin):
         idle_control_t2.mode = 2
         error_recovery_goal = ErrorRecoveryActionGoal()
 
-        if self.error_recov_client.get_state() is GoalStatus.ACTIVE:
+        if self.error_recov_client.wait_for_server(rospy.Duration(1.0)):
             self.idle_proxy(idle_control_l0)
             self.idle_proxy(idle_control_r0)
             self.idle_proxy(idle_control_t0)
@@ -385,11 +379,10 @@ class AssemblyGuiPlugin(Plugin):
             self.error_recov_client.send_goal_and_wait(error_recovery_goal)
             self._widget.textBrowser.append("UNLOCK_ERROR")
 
-        elif self.error_recov_client.get_state() is GoalStatus.LOST:
-            self._widget.textBrowser.append("SERVER LOST -- UNLOCK_ERROR")
+        else:
+            self._widget.textBrowser.append("SERVER NOT WORKING -- UNLOCK_ERROR")
 
-        elif self.error_recov_client.get_state() is GoalStatus.PENDING:
-            self._widget.textBrowser.append("SERVER PENDING -- UNLOCK_ERROR")
+
 
     def shutdown_plugin(self):
         # TODO unregister all publishers here
